db1eefc3804cc3065a2528caf37df09888369a76
orelch-eshkol <orel.chertkow@eshkol.com>
2024-07-01 11:23:54 +0300

    version 0.0.2

--- Package.swift
+++ Package.swift
@@ -5,19 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "MyCustomFramework",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyCustomFramework",
-            targets: ["MyCustomFramework"]),
+            targets: ["CustomFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyCustomFramework"),
-        .testTarget(
-            name: "MyCustomFrameworkTests",
-            dependencies: ["MyCustomFramework"]),
+//        .target(
+//            name: "MyCustomFramework"),
+//        .testTarget(
+//            name: "MyCustomFrameworkTests",
+//            dependencies: ["MyCustomFramework"]),
+        
+        .binaryTarget(
+            name: "CustomFramework",
+            path: "./Sources/CustomFramework.xcframework"
+        )
     ]
 )
